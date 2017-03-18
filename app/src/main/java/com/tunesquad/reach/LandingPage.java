f3c7991a33b16e5ce68070c9199cdbe3a27ab69c
loicmurumba <muruloic@gmail.com>
2017-03-17 23:43:37 -0400

    Created Project for austin

---
+++ app/src/main/java/com/tunesquad/reach/LandingPage.java
@@ -0,0 +1,13 @@
+package com.tunesquad.reach;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LandingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+    }
+}
