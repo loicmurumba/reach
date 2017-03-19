d3c9a1a242684effa07d101e4e1f531808d870e5
loicmurumba <muruloic@gmail.com>
2017-03-18 22:04:25 -0400

    design

--- app/src/main/java/com/tunesquad/reach/LandingPage.java
+++ app/src/main/java/com/tunesquad/reach/LandingPage.java
@@ -1,13 +1,56 @@
 package com.tunesquad.reach;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LandingPage extends AppCompatActivity {
+public class LandingPage extends AppCompatActivity implements View.OnClickListener {
+
+    private Button registerbutton;
+    private EditText passwordtext;
+    private EditText emailtext;
+    private ProgressDialog registering;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_landing_page);
+
+        registering = new ProgressDialog(this);
+        registerbutton = (Button) findViewById(R.id.registerbutton);
+        emailtext = (EditText) findViewById(R.id.emailtext);
+        passwordtext = (EditText) findViewById(R.id.passwordtext);
+
+
+
     }
+
+    @Override
+  public void onClick(View view){
+        if (view == registerbutton ){
+            String email = emailtext.getText().toString().trim();
+            String pass = passwordtext.getText().toString().trim();
+
+            if (TextUtils.isEmpty(email) || TextUtils.isEmpty(pass)) {
+
+                Toast.makeText(this, "Please fill in missing information", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+
+
+        }
+
+    }
+
 }
